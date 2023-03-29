9c30cbdfffe13b46de4b747fe4e3bbcc4cfad00c
zocom-christoffer-wallenberg <54267140+zocom-christoffer-wallenberg@users.noreply.github.com>
2023-03-29 11:37:00 +0200

    Added code

---
+++ example/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 8000
+
+const todoRouter = require('./routes/todo')
+
+app.use(express.json())
+
+app.use('/api/todo', todoRouter)
+
+app.listen(PORT, () => {
+  console.log('Server started')
+})
---
+++ example/routes/todo.js
@@ -0,0 +1,31 @@
+const { Router } = require('express');
+const router = Router();
+
+/**
+ * Hämta alla todos
+ * URL: /api/todo
+ * Method: GET
+ * 
+ * Lägga till en todo
+ * URL: /api/todo
+ * Method: POST
+ * body: {
+ *  username: Ada
+ *  password: pwd123
+ * }
+ * 
+ * Ta bort en todo
+ * URL: /api/todo/:id
+ * Method: DELETE
+ */
+
+const { checkBody, checkParams } = require('../middleware/index');
+const { get, add, remove } = require('../controllers/todoController');
+
+router.get('/', get);
+
+router.post('/', checkBody, add);
+
+router.delete('/:id', checkParams, remove);
+
+module.exports = router;
